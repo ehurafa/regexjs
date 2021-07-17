2c4b976f5397b0e80f600743654f65250d054b22
Rafael Gomes <rafarfl2010@gmail.com>
2021-07-16 20:18:34 -0400

    challenge #1

---
+++ characters/challenge-file-list.js
@@ -0,0 +1,7 @@
+const text = 'lista de arquivos mp3: jazz.mp3,rock.mp3,podcast.mp3,blues.mp3'
+// # quantas referencias de .mp3 estão na string?
+
+console.log(text.match(/\.mp3/g).length)
+
+// no futuro...
+console.log(text.match(/\w+\.mp3/g))
